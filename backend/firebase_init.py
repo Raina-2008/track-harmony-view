144feff975086d4dc5360a04f9384df79e1afd66
prakhark316-debug <prakhark316@gmail.com>
2026-08-29 23:29:28 +0530

    Support Render environment variable for Firebase credentials

--- backend/firebase_init.py
+++ backend/firebase_init.py
@@ -1,7 +1,14 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate("serviceAccountKey.json")
+if os.environ.get("FIREBASE_CREDENTIALS_JSON"):
+    cred_dict = json.loads(os.environ["FIREBASE_CREDENTIALS_JSON"])
+    cred = credentials.Certificate(cred_dict)
+else:
+    cred = credentials.Certificate("serviceAccountKey.json")
+
 firebase_admin.initialize_app(cred, {
     "databaseURL": "https://train-train-80645-default-rtdb.asia-southeast1.firebasedatabase.app"
 })
